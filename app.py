28095116f32e24cab4e336c403c64db8ed5e9221
Andrew <awong21@stuy.edu>
2017-09-22 12:53:01 -0400

    Added work

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+my_app = Flask(__name__)
+
+@my_app.route('/')
+def root():
+    return 'This is the root'
+@my_app.route('/beep')
+def beep():
+    return 'this is beep'
+@my_app.route('/boop')
+def boop():
+    return 'this is boop'
+@my_app.route('/barp')
+def barp():
+    return 'this is barp'
+    
+if __name__ == '__main__':
+    my_app.debug = True
+    my_app.run()
